6bcd01f93a192550c215db1e5e5270950e28a554
Ángel González <aglezabad@gmail.com>
2017-05-11 15:43:46 +0200

    Added nginx configuration from docker/nginx:alpine

--- proxies/deploy-proxies.sh
+++ proxies/deploy-proxies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker service create --name proxies -p 80:80 -p 443:443 nginx
+docker service create --name proxies -p 80:80 -p 443:443 -v /var/docker/proxies/nginx:/etc/nginx:rw nginx:alpine
