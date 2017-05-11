9fe1919c9d311be53ff8909455af98db7888edef
Ángel González <aglezabad@gmail.com>
2017-05-11 15:50:03 +0200

    Added network overlay

--- proxies/deploy-proxies.sh
+++ proxies/deploy-proxies.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Create network
+docker network create -d overlay proxies
+# Create service
 docker service create --name proxies -p 80:80 -p 443:443 -v /var/docker/proxies/nginx:/etc/nginx:rw nginx:alpine
