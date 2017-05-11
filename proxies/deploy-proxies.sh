32edd957eed536b956f3665ee30ead5cde5a9ea5
Ángel González <aglezabad@gmail.com>
2017-05-11 15:57:45 +0200

    Added -v as --mount because it's in swarm mode

--- proxies/deploy-proxies.sh
+++ proxies/deploy-proxies.sh
@@ -3,4 +3,7 @@
 # Create network
 docker network create -d overlay proxies
 # Create service
-docker service create --name proxies -p 80:80 -p 443:443 -v /var/docker/proxies/nginx:/etc/nginx:rw nginx:alpine
+docker service create --name proxies \
+-p 80:80 -p 443:443 \
+--mount type=bind,src=/var/docker/proxies/nginx,dst=/etc/nginx \
+nginx:alpine
