6ae1ad88b42ea752df036fe2650d46822e8ee826
Ángel González <aglezabad@gmail.com>
2017-05-16 09:31:05 +0200

    New nginx configuration

--- proxies/deploy-proxies.sh
+++ proxies/deploy-proxies.sh
@@ -5,5 +5,5 @@ docker network create -d overlay proxies
 # Create service
 docker service create --name proxies \
 -p 80:80 -p 443:443 \
---mount type=bind,src=/var/docker/proxies/nginx/conf.d,dst=/etc/nginx/conf.d \
+--mount type=bind,src=/var/docker-unvx/proxies/nginx/conf.d,dst=/etc/nginx/conf.d \
 nginx:alpine
