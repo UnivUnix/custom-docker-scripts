1766374619e92dcf9fa33e3619fde69a44922cbc
Ángel González <aglezabad@gmail.com>
2017-05-16 09:32:27 +0200

    Changed deploy script¡

--- proxies/deploy-proxies.sh
+++ proxies/deploy-proxies.sh
@@ -6,4 +6,5 @@ docker network create -d overlay proxies
 docker service create --name proxies \
 -p 80:80 -p 443:443 \
 --mount type=bind,src=/var/docker-unvx/proxies/nginx/conf.d,dst=/etc/nginx/conf.d \
+--mount type=bind,src=/var/docker-unvx/proxies/nginx/ssl,dst=/etc/nginx/ssl \
 nginx:alpine
